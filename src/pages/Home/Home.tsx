d87b272c15613301a39cdccce0e6c51a923558a5
minSsan <msp1125@hanyang.ac.kr>
2023-08-18 20:22:04 +0900

    Fix: inputScreenLayout 수정 필요 - home screen 원상복귀

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,6 +13,7 @@ import { DateTimePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 import InputScreenLayout from "../../components/input-screen-layout/InputScreenLayout";
 import { Link } from "react-router-dom";
+import Layout from "../../components/Layout";
 
 function Home() {
   // ? scroll ref - 스크롤 조작을 위해 사용
@@ -178,177 +179,174 @@ function Home() {
 
   return (
     <>
-      {/* //* 촬영 시간 입력 모달창 */}
-      {isModalOpen && (
-        <Modal
-          title="촬영 시간 직접입력"
-          setIsModalOpen={setIsModalOpen}
-          modalStyle={{ position: "relative" }}
-        >
-          <DateTimePicker
-            className="datePicker"
-            format={`YYYY년 MM월 DD일 HH:mm`}
-            value={dayjs(dateInfo)}
-            maxDateTime={dayjs()}
-            slotProps={{ textField: { size: "small" } }}
-            onAccept={(value) => {
-              if (value) {
-                setDateInfo(new Date(value.toISOString()));
-                setIsModalOpen(false);
-              }
-            }}
-          />
-        </Modal>
-      )}
+      <Layout>
+        <div ref={scrollRef} className="homeContainer">
+          {/* //* 촬영 시간 입력 모달창 */}
+          {isModalOpen && (
+            <Modal
+              title="촬영 시간 직접입력"
+              setIsModalOpen={setIsModalOpen}
+              modalStyle={{ position: "relative" }}
+            >
+              <DateTimePicker
+                className="datePicker"
+                format={`YYYY년 MM월 DD일 HH:mm`}
+                value={dayjs(dateInfo)}
+                maxDateTime={dayjs()}
+                slotProps={{ textField: { size: "small" } }}
+                onAccept={(value) => {
+                  if (value) {
+                    setDateInfo(new Date(value.toISOString()));
+                    setIsModalOpen(false);
+                  }
+                }}
+              />
+            </Modal>
+          )}
 
-      <InputScreenLayout
-        ref={scrollRef}
-        title={"사진 업로드"}
-        subtitle={"홍수 발생 지역의 사진을 업로드 해주세요."}
-      >
-        {/* //* image input & image preview */}
-        <div className="previewContainer">
-          {inputImage ? (
-            // ? 이미지를 입력한 경우, 입력한 이미지를 띄운다.
-            <>
-              <div>
-                <img
-                  className="previewImg"
-                  ref={imagePreviewRef}
-                  src={inputImage}
-                  alt="입력 사진"
-                />
-                {/* //? 이미지 삭제 버튼 */}
-                <button className="removeImgBtn" onClick={handleRemoveButton} />
-              </div>
+          <div style={{ padding: "2.875rem 0" }}>
+            {/* //* title */}
+            <h1 className="screenTitle">사진 업로드</h1>
+            {/* //* subtitle */}
+            <h3 className="screenSubtitle" style={{ marginTop: "0.375rem" }}>
+              홍수 발생 지역의 사진을 업로드 해주세요.
+            </h3>
 
-              {/* //* 촬영 시간 정보 */}
-              {/* //? 촬영 시간 제목 */}
-              <h3
-                className="screenSmallTitle"
-                style={{ marginTop: "1.375rem" }}
-              >
-                촬영 시간
-              </h3>
-
-              {/* //? 촬영 시간 텍스트 */}
-              <div style={{ marginTop: "0.375rem" }}>
-                {dateInfo ? (
-                  <>
-                    <p className="dateText">{`${dateInfo.getFullYear()}년 ${
-                      dateInfo.getMonth() + 1
-                    }월 ${dateInfo.getDate()}일 ${
-                      dateInfo.getHours() < 10
-                        ? "0" + dateInfo.getHours()
-                        : dateInfo.getHours()
-                    }:${
-                      dateInfo.getMinutes() < 10
-                        ? "0" + dateInfo.getMinutes()
-                        : dateInfo.getMinutes()
-                    }`}</p>
-                    {/* //? 촬영 시간 수정 안내 및 버튼 */}
-                    <span
-                      className="dateInputInfoContainer"
-                      style={{ marginTop: "0.25rem" }}
-                      onClick={() => setIsModalOpen(true)}
-                    >
-                      {/* 촬영 시간 수정 안내 */}
-                      <img className="questionMark" src={QuestionIcon} alt="" />
-                      <p style={{ marginLeft: "0.188rem" }}>
-                        사진의 실제 촬영 시간과 다른가요?
-                      </p>
-
-                      {/* 촬영 시간 수정 버튼 */}
-                      <EditButton style={{ marginLeft: "0.188rem" }} />
-                    </span>
-                  </>
-                ) : (
-                  <>
-                    <p
-                      className="fontRegular font14"
-                      style={{ color: "#F96A6A" }}
-                    >
-                      촬영 시간을 불러올 수 없습니다.
-                    </p>
-                    <span
-                      className="fontBold font12"
-                      style={{ color: "#F96A6A" }}
-                    >
-                      시간을 직접 입력해주세요.
-                      <EditButton
-                        style={{ marginLeft: "0.188rem" }}
-                        onClick={() => setIsModalOpen(true)}
-                      />
-                    </span>
-                  </>
-                )}
-              </div>
-
-              {isAnalyzed && (
+            {/* //* image input & image preview */}
+            <div className="previewContainer">
+              {inputImage ? (
+                // ? 이미지를 입력한 경우, 입력한 이미지를 띄운다.
                 <>
-                  {/* //* 홍수 분석 결과 */}
+                  <div>
+                    <img
+                      className="previewImg"
+                      ref={imagePreviewRef}
+                      src={inputImage}
+                      alt="입력 사진"
+                    />
+                    {/* //? 이미지 삭제 버튼 */}
+                    <button
+                      className="removeImgBtn"
+                      onClick={handleRemoveButton}
+                    />
+                  </div>
+
+                  {/* //* 촬영 시간 정보 */}
+                  {/* //? 촬영 시간 제목 */}
                   <h3
                     className="screenSmallTitle"
-                    style={{
-                      marginTop: "1.25rem",
-                      marginBottom: "0.375rem",
-                    }}
+                    style={{ marginTop: "1.375rem" }}
                   >
-                    분석 결과
+                    촬영 시간
                   </h3>
-                  {floodResult ? (
-                    <span className="resultTextContainer">
-                      침수 -&nbsp;
-                      <span className="resultText">{`${floodResult.floodLevel} ${floodResult.accuracy}%`}</span>
-                    </span>
-                  ) : (
-                    <span className="floodErrorText">
-                      홍수 발생 지역의 사진이 아닌 것 같습니다.
-                      <p>사진을 다시 입력해주세요.</p>
-                    </span>
+
+                  {/* //? 촬영 시간 텍스트 */}
+                  <div style={{ marginTop: "0.375rem" }}>
+                    {dateInfo ? (
+                      <>
+                        <p className="dateText">{`${dateInfo.getFullYear()}년 ${
+                          dateInfo.getMonth() + 1
+                        }월 ${dateInfo.getDate()}일 ${
+                          dateInfo.getHours() < 10
+                            ? "0" + dateInfo.getHours()
+                            : dateInfo.getHours()
+                        }:${
+                          dateInfo.getMinutes() < 10
+                            ? "0" + dateInfo.getMinutes()
+                            : dateInfo.getMinutes()
+                        }`}</p>
+                        {/* //? 촬영 시간 수정 안내 및 버튼 */}
+                        <span
+                          className="dateInputInfoContainer"
+                          style={{ marginTop: "0.25rem" }}
+                          onClick={() => setIsModalOpen(true)}
+                        >
+                          {/* 촬영 시간 수정 안내 */}
+                          <img
+                            className="questionMark"
+                            src={QuestionIcon}
+                            alt=""
+                          />
+                          <p style={{ marginLeft: "0.188rem" }}>
+                            사진의 실제 촬영 시간과 다른가요?
+                          </p>
+
+                          {/* 촬영 시간 수정 버튼 */}
+                          <EditButton style={{ marginLeft: "0.188rem" }} />
+                        </span>
+                      </>
+                    ) : (
+                      <>
+                        <p
+                          className="fontRegular font14"
+                          style={{ color: "#F96A6A" }}
+                        >
+                          촬영 시간을 불러올 수 없습니다.
+                        </p>
+                        <span
+                          className="fontBold font12"
+                          style={{ color: "#F96A6A" }}
+                        >
+                          시간을 직접 입력해주세요.
+                          <EditButton
+                            style={{ marginLeft: "0.188rem" }}
+                            onClick={() => setIsModalOpen(true)}
+                          />
+                        </span>
+                      </>
+                    )}
+                  </div>
+
+                  {isAnalyzed && (
+                    <>
+                      {/* //* 홍수 분석 결과 */}
+                      <h3
+                        className="screenSmallTitle"
+                        style={{
+                          marginTop: "1.25rem",
+                          marginBottom: "0.375rem",
+                        }}
+                      >
+                        분석 결과
+                      </h3>
+                      {floodResult ? (
+                        <span className="resultTextContainer">
+                          침수 -&nbsp;
+                          <span className="resultText">{`${floodResult.floodLevel} ${floodResult.accuracy}%`}</span>
+                        </span>
+                      ) : (
+                        <span className="floodErrorText">
+                          홍수 발생 지역의 사진이 아닌 것 같습니다.
+                          <p>사진을 다시 입력해주세요.</p>
+                        </span>
+                      )}
+                    </>
                   )}
                 </>
+              ) : (
+                // ? 이미지를 입력하지 않은 경우, 이미지 입력 버튼을 띄운다.
+                <>
+                  <label htmlFor="file">
+                    {/* //? 이미지 추가 box - 점선 background */}
+                    <div className="imageFileInputLabel"></div>
+                  </label>
+                  <input
+                    id="file"
+                    ref={imageInputRef}
+                    type="file"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                  />
+                </>
               )}
-            </>
-          ) : (
-            // ? 이미지를 입력하지 않은 경우, 이미지 입력 버튼을 띄운다.
-            <>
-              <label htmlFor="file">
-                {/* //? 이미지 추가 box - 점선 background */}
-                <div className="imageFileInputLabel">
-                  {/* //? 이미지 추가(+) 아이콘 */}
-                  <svg
-                    width={"3.75rem"}
-                    height={"3.75rem"}
-                    viewBox="0 0 60 60"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M56.6667 33.3333H3.33333C1.51111 33.3333 0 31.8222 0 30C0 28.1778 1.51111 26.6667 3.33333 26.6667H56.6667C58.4889 26.6667 60 28.1778 60 30C60 31.8222 58.4889 33.3333 56.6667 33.3333Z"
-                      fill="#BEBEBF"
-                    />
-                    <path
-                      d="M30 60C28.1778 60 26.6667 58.4889 26.6667 56.6667V3.33334C26.6667 1.51112 28.1778 7.62939e-06 30 7.62939e-06C31.8222 7.62939e-06 33.3333 1.51112 33.3333 3.33334V56.6667C33.3333 58.4889 31.8222 60 30 60Z"
-                      fill="#BEBEBF"
-                    />
-                  </svg>
-                </div>
-              </label>
-              <input
-                id="file"
-                ref={imageInputRef}
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-              />
-            </>
-          )}
+            </div>
+          </div>
         </div>
-
         {/* //? submit button (분석 시작 버튼) */}
-        <SubmitButton />
-      </InputScreenLayout>
+        <div className="submitBtnContainer">
+          <SubmitButton />
+        </div>
+      </Layout>
     </>
   );
 }
--- src/pages/InputLocation/InputLocation.tsx
+++ src/pages/InputLocation/InputLocation.tsx
@@ -4,14 +4,17 @@ import { Link } from "react-router-dom";
 
 function InputLocation() {
   return (
-    <InputScreenLayout
-      title="위치 선택"
-      subtitle="업로드한 사진의 촬영 위치를 선택해주세요."
-    >
-      <Link to={"/complete"} className="submitBtn active">
-        접수하기
-      </Link>
-    </InputScreenLayout>
+    <>
+      <InputScreenLayout
+        title="위치 선택"
+        subtitle="업로드한 사진의 촬영 위치를 선택해주세요."
+      ></InputScreenLayout>
+      <div className="submitBtnContainer">
+        <Link to={"/complete"} className="submitBtn active">
+          접수하기
+        </Link>
+      </div>
+    </>
   );
 }
 
